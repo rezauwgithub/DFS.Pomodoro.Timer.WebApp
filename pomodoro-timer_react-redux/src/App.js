419757ba8e56ee3f151522060af1e6f811b82f57
Reza Naeemi <rezauw@uw.edu>
2018-05-05 23:06:09 -0700

    Added file for App Settings, as well as react-helmet to make App Title in html file dynamic.

--- pomodoro-timer_react-redux/src/App.js
+++ pomodoro-timer_react-redux/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+
+import * as settings from './__app_settings__';
+
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <title>{settings.APP_TITLE}</title>
+        </Helmet>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ pomodoro-timer_react-redux/src/__app_settings__.js
@@ -0,0 +1,6 @@
+export const APP_TITLE = 'DFS Pomodoro Timer Web App';
+export const PRODUCT_VERSION = '1.0';
+export const VENDOR = 'DFS';
+export const HOME_PAGE = 'www.dfs.com';
+export const COPYRIGHT_YEARS = '2018-2019';
+export const DESCRIPTION = 'Pomodoro Timer';
