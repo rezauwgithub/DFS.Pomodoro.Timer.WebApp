51e9d3f328eed9e47480fc7177d7f30ae6100af0
Reza Naeemi <rezauw@uw.edu>
2018-05-13 15:13:43 -0700

    Commting TimerContainer.js implementation

---
+++ pomodoro-timer_react-redux/src/components/containers/TimerContainer.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import Timer from '../presentational/Timer';
+
+
+const mapStateToProps = (state) => {
+  return {
+    minutesPerRound: state.appdashboard.minutesPerRound,
+    minutesPerShortBreak: state.appdashboard.minutesPerShortBreak,
+    minutesPerLongBreak: state.appdashboard.minutesPerLongBreak,
+    numOfRounds: state.appdashboard.numOfRounds,
+    isRunning: state.appdashboard.isRunning
+  }
+};
+
+
+export default connect(
+  mapStateToProps, 
+)(Timer);
